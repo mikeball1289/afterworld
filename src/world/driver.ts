54e26865ed8468e9bea1831302d783651cb6799f
mike.ball@mikutech.com <mike.ball@mikutech.com>
2017-07-27 17:19:47 +0000

    fixed ramps

--- src/world/driver.ts
+++ src/world/driver.ts
@@ -33,11 +33,34 @@ function testLine(start: PointLike, end: PointLike, test: (x: number, y: number)
 // move an actor through the word, returns a pair of booleans representing
 // [horizontal collision occurred, vertical collision occurred]
 function move(actor: Actor, map: Map): [boolean, boolean] {
-    let magnitude = Math.ceil(Math.sqrt(actor.velocity.x * actor.velocity.x + actor.velocity.y * actor.velocity.y) * 2);
+    let magnitude = Math.ceil(Math.sqrt(actor.velocity.x * actor.velocity.x + actor.velocity.y * actor.velocity.y));
     let movingX = actor.velocity.x !== 0;
     let movingY = actor.velocity.y !== 0;
     let collisions: [boolean, boolean] = [false, false];
     for (let i = 0; i < magnitude && (movingX || movingY); i ++) {
+        if (movingY) {
+            actor.position.y += actor.velocity.y / magnitude;
+            if (actor.velocity.y < 0) {
+                // iunno
+            } else {
+                // if (Map.isWalkable(map.getPixelData(actor.left, actor.bottom - EPSILON)) ||
+                //     Map.isWalkable(map.getPixelData(actor.right - EPSILON, actor.bottom - EPSILON)))
+                if (testLine({ x: actor.left, y: actor.bottom - EPSILON }, { x: actor.right - EPSILON, y: actor.bottom - EPSILON },
+                    (x, y) => Map.isWalkable(map.getPixelData(x, y)), 2 ) &&
+                    !testLine({ x: actor.left, y: actor.bottom - 2 }, { x: actor.right - EPSILON, y: actor.bottom - 2 }, // if we're inside of a passable solid, then we haven't actually gotten through
+                    (x, y) => Map.isPassable(map.getPixelData(x, y)), actor.right - actor.left))
+                {
+                    if (testLine({ x: actor.left, y: actor.bottom - EPSILON }, { x: actor.right - EPSILON, y: actor.bottom - EPSILON },
+                        (x, y) => Map.isPassableRamp(map.getPixelData(x, y)), 2 ))
+                    {
+                        movingX = false;
+                    }
+                    movingY = false;
+                    collisions[1] = true;
+                    actor.position.y = Math.floor(actor.position.y);
+                }
+            }
+        }
         if (movingX) {
             actor.position.x += actor.velocity.x / magnitude;
             if (actor.velocity.x < 0) {
@@ -78,42 +101,25 @@ function move(actor: Actor, map: Map): [boolean, boolean] {
                 }
             }
         }
-        if (movingY) {
-            actor.position.y += actor.velocity.y / magnitude;
-            if (actor.velocity.y < 0) {
-                // iunno
-            } else {
-                // if (Map.isWalkable(map.getPixelData(actor.left, actor.bottom - EPSILON)) ||
-                //     Map.isWalkable(map.getPixelData(actor.right - EPSILON, actor.bottom - EPSILON)))
-                if (testLine({ x: actor.left, y: actor.bottom - EPSILON }, { x: actor.right - EPSILON, y: actor.bottom - EPSILON },
-                    (x, y) => Map.isWalkable(map.getPixelData(x, y)), 2 ) &&
-                    !testLine({ x: actor.left, y: actor.bottom - 1 - EPSILON }, { x: actor.right - EPSILON, y: actor.bottom - 1 - EPSILON }, // if we're inside of a passable solid, then we haven't actually gotten through
-                    (x, y) => Map.isPassable(map.getPixelData(x, y)), actor.right - actor.left))
-                {
-                    if (testLine({ x: actor.left, y: actor.bottom - EPSILON }, { x: actor.right - EPSILON, y: actor.bottom - EPSILON },
-                        (x, y) => Map.isPassableRamp(map.getPixelData(x, y)), 2 ))
-                    {
-                        movingX = false;
-                    }
-                    movingY = false;
-                    collisions[1] = true;
-                    actor.position.y = Math.floor(actor.position.y);
-                }
-            }
-        }
     }
     return collisions;
+}
+
+function isGrounded(actor: Actor, map: Map) {
+    return actor.velocity.y >= 0 &&
+        (testLine({ x: actor.left, y: actor.bottom }, { x: actor.right - EPSILON, y: actor.bottom  },
+        (x, y) => Map.isSolid(map.getPixelData(x, y)), 2) ||
+        (testLine({ x: actor.left, y: actor.bottom }, { x: actor.right - EPSILON, y: actor.bottom  },
+        (x, y) => Map.isWalkable(map.getPixelData(x, y)), 2) &&
+        !testLine({ x: actor.left, y: actor.bottom - 2 }, { x: actor.right - EPSILON, y: actor.bottom - 2 }, // if we're inside of a passable solid, then we haven't actually gotten through
+        (x, y) => Map.isPassable(map.getPixelData(x, y)), actor.right - actor.left)));
 }
 
 export default function update(world: World) {
     let player = world.player;
     let map = world.map;
     // the player is grounded if they aren't moving upwards, and their left or right foot is touching a walkable pixel
-    let grounded = player.velocity.y >= 0 &&
-                    testLine({ x: player.left, y: player.bottom }, { x: player.right - EPSILON, y: player.bottom  },
-                    (x, y) => Map.isWalkable(map.getPixelData(x, y)), 2 ) &&
-                    !testLine({ x: player.left, y: player.bottom - 1 }, { x: player.right - EPSILON, y: player.bottom - 1 }, // if we're inside of a passable solid, then we haven't actually gotten through
-                    (x, y) => Map.isPassable(map.getPixelData(x, y)), player.right - player.left);
+    let grounded = isGrounded(player, map);
 
     if (grounded) {
         if (keyboard.isKeyDown(Key.LEFT)) {
