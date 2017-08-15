447597d1c1fd248c4be33d0295c271c2e2b90128
mike.ball@mikutech.com <mike.ball@mikutech.com>
2017-08-15 16:03:50 +0000

    updated ignore to exclude splits

--- art/pack.bat
+++ art/pack.bat
@@ -1,3 +1,5 @@
+pushd split
 mkdir %1
-\cygwin64\bin\convert %1.png -crop 4x8@ -set filename:tile "%%[fx:page.y/128]-%%[fx:page.x/256]" +repage +adjoin %1/%%[filename:tile].png
-"\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" --format json --data out/%1.json --sheet out/%1.png --trim-sprite-names --algorithm MaxRects --disable-rotation ./%1
+popd
+\cygwin64\bin\convert %1.png -crop 4x8@ -set filename:tile "%%[fx:page.y/128]-%%[fx:page.x/256]" +repage +adjoin ./split/%1/%%[filename:tile].png
+"\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" --format json --data out/%1.json --sheet out/%1.png --trim-sprite-names --algorithm MaxRects --disable-rotation ./split/%1
